2399ad1c0d1d49f58bac6bca178e6636cc8b5221
danielcortesao <danielccortesao6@gmail.com>
2019-04-16 16:49:20 +0100

    Instaciação de particulas A e B

---
+++ Absolute Zero/Assets/Scripts/generateA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateA : MonoBehaviour   
+{
+
+    public GameObject A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 currentPosition = new Vector3(0f, 0f, -1f);//A.transform.position;
+
+        float minX = -8.0f;
+        float maxX = 8.0f;
+        float minY = -4.0f;
+        float maxY = 4.0f;
+        float Z = -1.0f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            
+            //GameObject tmpObj = GameObject.Instantiate(A, currentPosition, Quaternion.identity) as GameObject;
+            //currentPosition += new Vector3(1f, 0f, 0f);
+            
+
+
+            Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Z);
+            Instantiate(A, position, Quaternion.identity);
+
+        }
+
+        
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
